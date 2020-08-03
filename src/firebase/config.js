9467be50108403c071c2bd3980480913e7afc8de
paslavskyi9roman <paslavsky.roman@gmail.com>
2020-08-03 19:43:43 +0300

    innit

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: 'AIzaSyAvoSlLlgwDXSoRKWSW61IRBjMo5ClYKMI',
+  authDomain: 'rom-firegram.firebaseapp.com',
+  databaseURL: 'https://rom-firegram.firebaseio.com',
+  projectId: 'rom-firegram',
+  storageBucket: 'rom-firegram.appspot.com',
+  messagingSenderId: '18051051212',
+  appId: '1:18051051212:web:ceba7946f23af6bbfe5c69',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+ 
+export (projectStorage, projectFirestore);
